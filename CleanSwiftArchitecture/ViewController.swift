dbb6222ac86c20b60929e3fbe05238f816d628ee
Mahipal Singh <mahipalvarkana543@gmail.com>
2019-07-09 22:15:23 +0530

    Initial Commit

---
+++ CleanSwiftArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftArchitecture
+//
+//  Created by india on 09/07/19.
+//  Copyright © 2019 Mahipal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
